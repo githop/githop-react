8988d354161d66e49a387887bf0986d8d7385c73
githop <tom@githop.com>
2018-01-11 20:00:17 -0700

    updates(resume): enable redux devtools

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,9 @@
 import { applyMiddleware, compose, createStore, Store } from 'redux';
 import reduxThunk from 'redux-thunk';
 import { IState, state } from './reducers';
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store: Store<IState> = createStore(
   state,
-  compose(applyMiddleware(reduxThunk))
+  composeEnhancers(applyMiddleware(reduxThunk))
 );
