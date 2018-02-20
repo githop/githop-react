46a7762d2f55744720bab8f76774e0fb8231ee23
githop <tom@githop.com>
2018-02-19 17:22:36 -0700

    updates(service worker): dispatch tooltips for sw events

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,11 @@ import {
   Switch
 } from 'react-router-dom';
 import * as Loadable from 'react-loadable';
-import { Provider } from 'react-redux';
-import { store } from './store';
 import AuthenticatedRoute from './components/AuthenticatedRoute';
 import Home from './components/home/Home';
 import Loading from './components/Loading/Loading';
 import TooltipContainer from './containers/TooltipContainer';
+
 const Resume = Loadable({
   loader: () => import('./containers/ResumeContainer'),
   loading: () => <Loading/>
@@ -37,42 +36,40 @@ const AddResumeCard = Loadable({
 class App extends React.Component {
   render() {
     return (
-        <Provider store={store}>
-          <Router>
-            <div className="gth-root">
-              <TooltipContainer/>
-              <div className="gth-content">
-                <div className="gth-nav-buffer">
-                  <nav className="gth-nav">
-                    <NavLink className="gth-nav-link" to="/">
-                      <span className="--gth-emoji">🏠</span>
-                      <br/>
-                      <span className="link-text">Home</span>
-                    </NavLink>
-                    <NavLink to="/resume">
-                      <span className="--gth-emoji">💼</span>
-                      <br/>
-                      <span className="link-text">Resume</span>
-                    </NavLink>
-                  </nav>
-                </div>
-
-                <Switch>
-                  <Route exact={true} path="/" component={Home}/>
-                  <Route exact={true} path="/resume" component={Resume}/>
-                  <Route exact={true} path="/login" component={Login}/>
-                  <AuthenticatedRoute exact={true} path="/resume/add" component={AddResumeCard}/>
-                  <AuthenticatedRoute path="/resume/:id/edit" component={ResumeEditor}/>
-                </Switch>
+        <Router>
+          <div className="gth-root">
+            <TooltipContainer/>
+            <div className="gth-content">
+              <div className="gth-nav-buffer">
+                <nav className="gth-nav">
+                  <NavLink className="gth-nav-link" to="/">
+                    <span className="--gth-emoji">🏠</span>
+                    <br/>
+                    <span className="link-text">Home</span>
+                  </NavLink>
+                  <NavLink to="/resume">
+                    <span className="--gth-emoji">💼</span>
+                    <br/>
+                    <span className="link-text">Resume</span>
+                  </NavLink>
+                </nav>
               </div>
-              <footer className="gth-footer">
-                Built with love by&nbsp;
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/githop/githop-react">githop</a>
-                <span><Link to="/login">Admin</Link></span>
-              </footer>
+
+              <Switch>
+                <Route exact={true} path="/" component={Home}/>
+                <Route exact={true} path="/resume" component={Resume}/>
+                <Route exact={true} path="/login" component={Login}/>
+                <AuthenticatedRoute exact={true} path="/resume/add" component={AddResumeCard}/>
+                <AuthenticatedRoute path="/resume/:id/edit" component={ResumeEditor}/>
+              </Switch>
             </div>
-          </Router>
-        </Provider>
+            <footer className="gth-footer">
+              Built with love by&nbsp;
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/githop/githop-react">githop</a>
+              <span><Link to="/login">Admin</Link></span>
+            </footer>
+          </div>
+        </Router>
     );
   }
 }
